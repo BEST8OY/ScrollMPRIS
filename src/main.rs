00fcdb2d349c7bb68ce1b7b3bee58c20c26bd495
Alireza Jr <70865987+BEST8OY@users.noreply.github.com>
2025-04-11 05:12:04 +0330

    hh:mm:ss to h:mm:ss

--- src/main.rs
+++ src/main.rs
@@ -52,7 +52,7 @@ mod mpris {
             let hours = total_seconds / 3600;
             let minutes = (total_seconds % 3600) / 60;
             let seconds = total_seconds % 60;
-            format!("{:02}:{:02}:{:02}", hours, minutes, seconds)
+            format!("{:01}:{:02}:{:02}", hours, minutes, seconds)
         } else {
             let minutes = total_seconds / 60;
             let seconds = total_seconds % 60;
